0427331841d5497a540f5a1e141ddbfb10443efa
Cass Dietz <cassd@umich.edu>
2026-01-30 21:27:59 -0500

    init and str

--- HW3.py
+++ HW3.py
@@ -1,6 +1,6 @@
-# Name:
-# Student ID:
-# Email:
+# Name: Cass Dietz
+# Student ID: 66530638
+# Email: cassd@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
@@ -33,7 +33,10 @@ class CouponDispenser:
             coupon_cards (list[str]): list of possible coupons users can receive.
         """
         # TODO: Implement per instructions
-        pass
+        
+        self.coupon_cards = coupon_cards
+        self.customer_roster = []
+        self.issued_indices = []
 
     def __str__(self):
         """
@@ -44,7 +47,11 @@ class CouponDispenser:
             str
         """
         # TODO: Implement per instructions
-        pass
+        if not self.coupon_cards:
+            return ""
+        else:
+            return "|".join(self.coupon_cards)
+    
 
     def issue_coupon(self, name):
         """
